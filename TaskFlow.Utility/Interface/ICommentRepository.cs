0c39997a71c235e3edd638fef461d4da37b935ef
divacs <sonja.divac@yahoo.com>
2025-09-23 15:28:40 +0200

    Add RepositoryPattern for Comment

---
+++ TaskFlow.Utility/Interface/ICommentRepository.cs
@@ -0,0 +1,14 @@
+﻿using TaskFlow.Models.Models;
+
+namespace TaskFlow.Utility.Interface
+{
+    public interface ICommentRepository
+    {
+        Task<IEnumerable<Comment>> GetAllAsync();
+        Task<Comment?> GetByIdAsync(int id);
+        Task<IEnumerable<Comment>> GetByProjectIdAsync(int projectId);
+        Task<IEnumerable<Comment>> GetByTaskItemIdAsync(int taskItemId);
+        Task AddAsync(Comment comment);
+        Task DeleteAsync(int id);
+    }
+}
---
+++ TaskFlow.Utility/Repository/CommentRepository.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskFlow.Models.Models;
+using TaskFlow.Utility.Interface;
+using TaskFlow.Data;
+
+namespace TaskFlow.Utility.Repositories
+{
+    public class CommentRepository : ICommentRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CommentRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Comment>> GetAllAsync()
+        {
+            return await _context.Comments
+                .Include(c => c.Project)
+                .Include(c => c.TaskItem)
+                .ToListAsync();
+        }
+
+        public async Task<Comment?> GetByIdAsync(int id)
+        {
+            return await _context.Comments
+                .Include(c => c.Project)
+                .Include(c => c.TaskItem)
+                .FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task<IEnumerable<Comment>> GetByProjectIdAsync(int projectId)
+        {
+            return await _context.Comments
+                .Where(c => c.ProjectId == projectId)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Comment>> GetByTaskItemIdAsync(int taskItemId)
+        {
+            return await _context.Comments
+                .Where(c => c.TaskItemId == taskItemId)
+                .ToListAsync();
+        }
+
+        public async Task AddAsync(Comment comment)
+        {
+            _context.Comments.Add(comment);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var comment = await GetByIdAsync(id);
+            if (comment != null)
+            {
+                _context.Comments.Remove(comment);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
